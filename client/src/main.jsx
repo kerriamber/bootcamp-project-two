e379299191e2a899f85826b1f470951c21a22484
Jeff <jlc68@me.com>
2025-02-13 17:27:13 -0600

    maybe this time

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage.jsx";
+import Login from "./pages/Login.jsx";
+import Post from "./pages/Post.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// TODO: import bootstrap
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    children: [
+      //TODO: place the rest of the page route info here:
+      {
+        index: true,
+        element: <Login />,
+      },
+      {
+        path: "/post",
+        element: <Post />,
+      },
+    ],
+  },
+]);
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
