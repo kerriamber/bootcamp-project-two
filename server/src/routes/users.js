5f1cec7d7720f695df275f4ad8fb92ee9c4d6b85
Kerri Salciccioli <kerri@salciccio.li>
2025-02-24 16:04:00 -0500

    dont return pw on create user

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -6,18 +6,14 @@ const router = Router();
 router.post("/", async (req, res) => {
     try {        
         const userData = await User.create(req.body);
-        res.status(200).json(
-            userData
-        );
+        res.status(200).json({
+            id: userData.id,
+            username: userData.username,
+            email: userData.email
+    });
     } catch (err) {
-        console.log(err);
         res.status(400).json(err);
     }
-});
-
-router.get("/", async (_req, res) => {
-    const users = await User.findAll();
-    res.status(200).json(users);
 });
 
 export { router as userRoutes };
