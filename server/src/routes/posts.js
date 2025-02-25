c6124ddac60cd542eb34b2bcbefb4ae501d328b5
Kerri Salciccioli <kerri@salciccio.li>
2025-02-24 20:54:48 -0500

    barebones post routes

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,4 +1,22 @@
 import { Router } from "express";
+import { auth } from "../middleware/authorization.js";
+
 const router = Router();
+
+// get multiple posts
+router.get("/", async (req, res) => {
+    
+});
+
+// get post with id
+router.get("/:id", async (req, res) => {
+
+});
+
+// create a post
+router.post("/", auth, async (req, res) => {
+    // temp just to test auth middleware
+    res.status(200).send('auth success');
+});
 
 export { router as postsRoutes };
