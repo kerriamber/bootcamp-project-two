53e8d0175365c5c46be4e892ff30fda854a5dbe2
ikealmighty <isaacyates7@gmail.com>
2025-02-24 19:34:02 -0600

    added joke fetch wrapper and added image and joke to page

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,13 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import TextBox from "../components/TextBox";
+import { getRandomJoke } from "../api/joke";
+import { getRandomCoffeeSrc } from "../api/coffee";
+import { useState, useEffect } from "react";
+import { useNavigation } from "react-router-dom";
 
 export default function Post() {
-  const handlePostSubmit = (content) => {
-    console.log("Great! Your Post has been submitted:", content);
-  };
+  const navigation = useNavigation();
+
+  const [joke, setJoke] = useState();
+  const [coffeeSrc, setCoffeeSrc] = useState();
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    async function loadData() {
+      setJoke(await getRandomJoke());
+      setCoffeeSrc(await getRandomCoffeeSrc());
+    }
+
+    loadData();
+  }, []);
+
+  const handlePostSubmit = () => {};
 
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+      <div className="card shadow rounded-4 overflow-hidden col-lg-6 h-100 flex align-items-center">
+        <div className="m-5 fst-italic fs-3">{joke}</div>
+        <img className="pb-5" src={coffeeSrc} alt="coffee image" />
+      </div>
       <TextBox onSubmit={handlePostSubmit} />
     </div>
   );
