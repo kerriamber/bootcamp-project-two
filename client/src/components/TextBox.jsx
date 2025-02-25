ce488d601fdd3e0b30fa54feaecf90ba22871287
Jeff <jlc68@me.com>
2025-02-24 21:40:08 -0600

    Added the reflection.

--- client/src/components/TextBox.jsx
+++ client/src/components/TextBox.jsx
@@ -36,7 +36,7 @@ export default function TextBox({ onSubmit }) {
 
   return (
     <div className="card shadow p-4 rounded-4 h-75 m-1 col-lg-6">
-      <h3 className="text-center">Create New Post</h3>
+      <h3 className="text-center">Create your reflection about the coffee and the joke:</h3>
       <form onSubmit={handleSubmit}>
         <input
           className="form-control mt-3"
